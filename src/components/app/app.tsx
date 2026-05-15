16ec9f0dce0488ae5de32fbe5fb1d4f887c419c8
uvarova-valeriya <v.p.uvarova21@gmail.com>
2026-05-15 12:58:02 +0300

    Функциональность реализована

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,34 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './../../constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './../../constants/articleProps';
 
 import styles from './app.module.scss';
 
 export const App = () => {
+	const [articleParams, setArticleParams] = useState(defaultArticleState);
+	const applyParms = (newParms: ArticleStateType) => {
+		setArticleParams(newParms);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleParams.fontFamilyOption.value,
+					'--font-size': articleParams.fontSizeOption.value,
+					'--font-color': articleParams.fontColor.value,
+					'--container-width': articleParams.contentWidth.value,
+					'--bg-color': articleParams.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm onApply={applyParms} />
 			<Article />
 		</main>
 	);
--- src/ui/story-decorator/StoryDecorator.tsx
+++ src/ui/story-decorator/StoryDecorator.tsx
@@ -1,4 +1,4 @@
-import { Decorator } from '@storybook/react';
+import { Decorator } from '@storybook/react-vite';
 import styles from './StoryDecorator.module.scss';
 
 export const StoryDecorator: Decorator = (Story) => (
